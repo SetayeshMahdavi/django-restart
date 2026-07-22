3a5b93e4e671156c0509904fbc1ca2cae860c0fe
Setayesh Mahdavi <setayesh.mahdavi04@gmail.com>
2026-07-22 19:27:44 +0330

    ......

---
+++ website/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+from .models import Contact
+
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = ['name', 'email', 'subject', 'message']
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'class': 'common-input mb-20 form-control',
+                'placeholder': 'Enter your name',
+            }),
+            'email': forms.EmailInput(attrs={
+                'class': 'common-input mb-20 form-control',
+                'placeholder': 'Enter email address',
+            }),
+            'subject': forms.TextInput(attrs={
+                'class': 'common-input mb-20 form-control',
+                'placeholder': 'Enter subject',
+            }),
+            'message': forms.Textarea(attrs={
+                'class': 'common-textarea form-control',
+                'placeholder': 'Enter Messege',
+            }),
+        }
